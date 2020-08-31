934e2a401e58a226cf9f08fccb251045fb4fee27
HAFIS LUEBAEKASENG (ฮาฟิซ ลือแบกาเส็ง) <6010110420@email.psu.ac.th>
2020-08-31 17:09:47 +0700

    part 6

--- screen/WeatherScreen.js
+++ screen/WeatherScreen.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, StatusBar, StyleSheet } from 'react-native'
 import Weather from '../compoents/Weather'
 
 
 export default function WeatherScreen(props){
     return(
-        <View>
+        <View style={styles.sss}>
             <Weather zipCode={props.route.params.zipCode}/>
+            <StatusBar style="auto" />
         </View>
     )
 }
+const styles = StyleSheet.create({
+    sss: {
+        width: '100%',
+        height: '100%',
+
+        
+    },
+})
