e92a73182aaa42e3ed83e96067115a4c27b26c79
HAFIS LUEBAEKASENG (ฮาฟิซ ลือแบกาเส็ง) <6010110420@email.psu.ac.th>
2020-08-31 14:18:25 +0700

    Part 2

--- App.js
+++ App.js
@@ -1,24 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Weather from './compoents/Weather';
+import Constants from 'expo-constants';
+
 export default function App() {
- const doIt = () => {
- console.log("Hello from console")
- }
- return (
- <View style={styles.container}>
- <Text onPress={doIt}>Hello world</Text>
- <StatusBar style="auto" />
- </View>
- );
+
+  return (
+    <View style={styles.container}>
+    <Weather zipCode="90110"/>
+    <StatusBar style="auto" />
+    </View>
+  );
 }
 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.StatusBartusBarHeight
   },
 });
---
+++ compoents/Weather.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { Text, ImageBackground, View, StyleSheet } from 'react-native'
+import Forcast from './Forcast';
+
+export default function Weather(props){
+    const [forecastInfo, setForecastInfo] = useState({
+        main: '-',
+        description: '-',
+        temp: 0
+        }) 
+    return (
+        <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
+                <Text>Zip Code</Text>
+                <Text>{props.zipCode}</Text>
+                <Forcast {...forecastInfo}/>
+        </ImageBackground>
+    )
+}
+
+const styles = StyleSheet.create({
+    backdrop: {
+        width: '100%',
+        height: '100%'
+    }
+})
